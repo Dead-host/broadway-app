84ba01db8e396b5145e261946c4a5c9d3a5c4b50
Sujal Sangraula <sujalsangraula5@gmail.com>
2025-07-10 13:47:21 +0545

    seller profile page

--- lib/buyerPart/profilePage.dart
+++ lib/buyerPart/profilePage.dart
@@ -85,104 +85,106 @@ class _ProfilepageState extends State<Profilepage> {
               ],
             ),
           ),
-          body: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 20,right: 20,top: 20),
-                child: TextFormField(
-                  controller: userNameController,
-                  decoration: InputDecoration(
-                      suffixIcon: Icon(Icons.drive_file_rename_outline),
-                      hintText: "Change User Name",
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20),
-                        borderSide: BorderSide(color: Colors.grey),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.green),
-                        borderRadius: BorderRadius.circular(20),
-                      )
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 20,right: 20,top: 20),
+                  child: TextFormField(
+                    controller: userNameController,
+                    decoration: InputDecoration(
+                        suffixIcon: Icon(Icons.drive_file_rename_outline),
+                        hintText: "Change User Name",
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: BorderSide(color: Colors.grey),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.green),
+                          borderRadius: BorderRadius.circular(20),
+                        )
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: 30,),
-              Padding(
-                padding: const EdgeInsets.only(left: 20,right: 20),
-                child: TextFormField(
-                  obscureText: see,
-                  controller: passwordChangeController,
-                  decoration: InputDecoration(
-                      suffixIcon: IconButton(
-                          onPressed: (){
-                            setState(() {
-                              see=!see;
-                            });
-                          },
-                          icon: Icon(see?Icons.visibility:Icons.visibility_off)),
-                      hintText: "Change Password",
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20),
-                        borderSide: BorderSide(color: Colors.grey),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.green),
-                        borderRadius: BorderRadius.circular(20),
-                      )
+                SizedBox(height: 30,),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20,right: 20),
+                  child: TextFormField(
+                    obscureText: see,
+                    controller: passwordChangeController,
+                    decoration: InputDecoration(
+                        suffixIcon: IconButton(
+                            onPressed: (){
+                              setState(() {
+                                see=!see;
+                              });
+                            },
+                            icon: Icon(see?Icons.visibility:Icons.visibility_off)),
+                        hintText: "Change Password",
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: BorderSide(color: Colors.grey),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.green),
+                          borderRadius: BorderRadius.circular(20),
+                        )
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: 30,),
-              Padding(
-                padding: const EdgeInsets.only(left: 20,right: 20),
-                child: TextFormField(
-                  obscureText: seePass,
-                  controller: confirmPasswordController,
-                  decoration: InputDecoration(
-                      suffixIcon: IconButton(
-                          onPressed: (){
-                            setState(() {
-                              seePass=!seePass;
-                            });
-                          },
-                          icon: Icon(seePass?Icons.visibility:Icons.visibility_off)),
-                      hintText: "Confirm Password",
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20),
-                        borderSide: BorderSide(color: Colors.grey),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.green),
-                        borderRadius: BorderRadius.circular(20),
-                      )
-                  ),
+                SizedBox(height: 30,),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20,right: 20),
+                  child: TextFormField(
+                    obscureText: seePass,
+                    controller: confirmPasswordController,
+                    decoration: InputDecoration(
+                        suffixIcon: IconButton(
+                            onPressed: (){
+                              setState(() {
+                                seePass=!seePass;
+                              });
+                            },
+                            icon: Icon(seePass?Icons.visibility:Icons.visibility_off)),
+                        hintText: "Confirm Password",
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: BorderSide(color: Colors.grey),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.green),
+                          borderRadius: BorderRadius.circular(20),
+                        )
+                    ),
 
+                  ),
                 ),
-              ),
-              SizedBox(height: 30,),
-              Padding(
-                padding: const EdgeInsets.only(left: 30,right: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        Text("Enable Biometric ",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
-                        Icon(Icons.fingerprint),
-                      ],
-                    ),
-                    Switch(
-                        value: light,
-                        activeColor: Colors.greenAccent,
-                        onChanged: (bool value){
-                          setState(() {
-                            light=value;
-                          });
-                        }
-                    ),
-                  ],
-                ),
-              )
-            ],
+                SizedBox(height: 30,),
+                Padding(
+                  padding: const EdgeInsets.only(left: 30,right: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Text("Enable Biometric ",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
+                          Icon(Icons.fingerprint),
+                        ],
+                      ),
+                      Switch(
+                          value: light,
+                          activeColor: Colors.greenAccent,
+                          onChanged: (bool value){
+                            setState(() {
+                              light=value;
+                            });
+                          }
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ));
   }
